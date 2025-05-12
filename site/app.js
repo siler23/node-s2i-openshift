cb450ac2da30ce5829d23df7d281a43ed92fdd3b
Garrett Woodworth <glwoodworth@ucdavis.edu>
2025-05-12 14:26:43 -0700

    Update app.js

--- site/app.js
+++ site/app.js
@@ -52,10 +52,10 @@ app.get('/info', function(req, res) {
   if (CURRENTMODE == MODE.TEST) {
     patientdata = {
       "personal": {
-        "name": "Ralph DAlmeida",
-        "age": 38,
-        "gender": "male",
-        "street": "34 Main Street",
+        "name": "Halo Michael",
+        "age": 19,
+        "gender": "female",
+        "street": "35 Main Street",
         "city": "Toronto",
         "zipcode": "M5H 1T1"
       },
